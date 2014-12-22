fec6b2d21f75f993b8a640778312c838f0bb0608
giotroni <giorgio@troni.it>
2014-12-22 19:21:48 +0100

    memorizzazione locale

--- js/index.js
+++ js/index.js
@@ -95,20 +95,8 @@ var app = {
     },
      
     deviceready: function() {
-        checkConnection();
         
-    },
-      checkConnection: function() {
-        var networkState = navigator.network.connection.type;
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
-        alert('Connection type: ' + states[networkState]);
+        
     }
 }
 app.intro= function (){
@@ -423,6 +411,20 @@ mappa.onErrorGeo = function(error) {
   messaggio("Attenzione!","Problemi di conenssione", "Ok");
 }
 
+function checkConnection() {
+  var networkState = navigator.network.connection.type;
+  var states = {};
+  states[Connection.UNKNOWN]  = 'Unknown connection';
+  states[Connection.ETHERNET] = 'Ethernet connection';
+  states[Connection.WIFI]     = 'WiFi connection';
+  states[Connection.CELL_2G]  = 'Cell 2G connection';
+  states[Connection.CELL_3G]  = 'Cell 3G connection';
+  states[Connection.CELL_4G]  = 'Cell 4G connection';
+  states[Connection.NONE]     = 'No network connection';
+  alert('Connection type: ' + states[networkState]);
+}
+
+
 $(document).ready(function() {
     app.initialize();
       if ( CORDOVA ) {
@@ -436,7 +438,7 @@ $(document).ready(function() {
             alert(app.user_data.nome + " " + app.user_data.id);
             $("#nome").html(app.user.data.nome)
           }
-  
+        checkConnection();  
       } else {
         URL_PREFIX = "";
       }
